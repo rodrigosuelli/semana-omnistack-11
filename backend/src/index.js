a75de461baf12a6db6c20bdcbc1dd35e851ade13
Rodrigo Suélli <rodrigosuellli@gmail.com>
2020-03-25 20:02:58 -0300

    Dia 2

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+const port = 3333;
+
+/**PRODUÇÃO */
+// app.use(cors({
+//     origin: 'http://meuapp.com'
+// }));
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
