e232e6e50956fafbd3b43a5c21d71c6c7f8679a3
Rodrigo Suélli <rodrigosuellli@gmail.com>
2020-03-29 17:39:51 -0300

    Dia 5

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
-const port = 3333;
 
 /**PRODUÇÃO */
 // app.use(cors({
@@ -12,5 +12,6 @@ const port = 3333;
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = app;
